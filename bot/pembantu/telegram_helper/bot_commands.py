324dcd155607d8b635bdf8473e7cd1d06814c38b
DulHaalim <69864107+DulHaalim@users.noreply.github.com>
2021-01-19 14:29:40 +0700

    Update and rename bot/helper/telegram_helper/bot_commands.py to bot/pembantu/telegram_helper/bot_commands.py

---
+++ bot/pembantu/telegram_helper/bot_commands.py
@@ -0,0 +1,22 @@
+class _BotCommands:
+    def __init__(self):
+        self.StartCommand = 'Dstart'
+        self.MirrorCommand = 'Dmirror'
+        self.UnzipMirrorCommand = 'Dunzip'
+        self.TarMirrorCommand = 'Dtar'
+        self.CancelMirror = 'Dcancel'
+        self.CancelAllCommand = 'Dcancelall'
+        self.ListCommand = 'Dfind'
+        self.StatusCommand = 'Dlist'
+        self.AuthorizeCommand = 'Dauth'
+        self.UnAuthorizeCommand = 'Dunauth'
+        self.PingCommand = 'Dping'
+        self.RestartCommand = 'Drestart'
+        self.StatsCommand = 'Dusage'
+        self.HelpCommand = 'Dhelp'
+        self.LogCommand = 'Dlog'
+        self.CloneCommand = "Dclone"
+        self.WatchCommand = 'Dwatch'
+        self.TarWatchCommand = 'Dtarwatch'
+
+BotCommands = _BotCommands()
